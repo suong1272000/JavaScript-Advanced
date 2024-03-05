a5b5a804d2077942f4fa67a847eef9422e50a7fd
kseonghee <kseonghee0127@gmail.com>
2024-03-05 17:29:38 +0900

    feat: 가로스크롤 공부

--- scroll/index.js
+++ scroll/index.js
@@ -1,11 +1,22 @@
-window.addEventListener("scroll", () => {
-	// 바디의 높이 찾기
-	const scrollY = window.scrollY;
-	// 내부 높이 찾기
-	const innerHeight = window.innerHeight;
-	// 브라우저 진짜 높이
-	const totalHeight = document.documentElement.scrollHeight;
-	// 스크롤된 화면 위치가 브라우저 높이보다 큰가?
-	console.log(scrollY + innerHeight >= totalHeight ? "나락" : "중간");
-	if (scrollY + innerHeight >= totalHeight) alert("끝!");
+document.querySelectorAll(".js-horizontal-scroll").forEach((el) => {
+	el.addEventListener("wheel", (e) => {
+		// 스크롤이 왼쪽 또는 오른쪽 끝에 도달했는지 확인
+		const atLeftEnd = el.scrollLeft === 0;
+		const atRightEnd = el.scrollLeft + el.offsetWidth === el.scrollWidth;
+
+		// 휠 이벤트가 위로 가는 것인지 아래로 가는 것인지 확인
+		const scrollingUp = e.deltaY < 0;
+		const scrollingDown = e.deltaY > 0;
+
+		if ((atLeftEnd && scrollingUp) || (atRightEnd && scrollingDown)) {
+			// 스크롤이 양 끝에 있고 사용자가 해당 방향으로 계속 스크롤하려고 하면,
+			// 이벤트의 기본 동작을 수행하여 수직 스크론을 허용합니다.
+			return;
+		}
+
+		// 그렇지 않으면, 가로 스크롤을 진행합니다.
+		e.preventDefault();
+		// noinspection JSSuspiciousNameCombination
+		el.scrollLeft += e.deltaY * 2;
+	});
 });
