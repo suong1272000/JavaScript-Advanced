60317bee7e30b9f596a3194676030f5727c17210
kseonghee <kseonghee0127@gmail.com>
2024-03-07 17:31:39 +0900

    feat: 비동기

---
+++ coffee/index.js
@@ -0,0 +1,13 @@
+const order = new Promise((res, rej) => {
+	const user = prompt("커피입력?", "아메리카노");
+	if (!!user) {
+		document.querySelector(".box").innerHTML = `${user}준비중`;
+		setTimeout(() => res(user), 3000);
+	} else {
+		document.querySelector(".box").innerHTML = "커피를 주문하지 않음";
+	}
+});
+
+const done = (user) =>
+	(document.querySelector(".box").innerHTML = `${user} 주문 완료`);
+order.then(done);
