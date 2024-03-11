fd2cd7756a7f98352a6d6ec5ccbc9c7d9108f786
kseonghee <kseonghee0127@gmail.com>
2024-03-11 17:39:50 +0900

    feat

--- async/index.js
+++ async/index.js
@@ -1,39 +1,20 @@
-//🥚🐣🐥🐓🍗
+const fetchData = async (num) => {
+	const addresses = await fetch(
+		`https://fakerapi.it/api/v1/addresses?_quantity=${num}`
+	);
+	const addressesData = await addresses.json();
+	const users = await fetch(
+		`https://fakerapi.it/api/v1/users?_quantity=${num}&_gender=male`
+	);
+	const userData = await users.json();
 
-//hatch grow roostar chicken makeChicken()
-const delay = () => new Promise((res) => setTimeout(() => res(), 1000));
+	const streetNames = addressesData.data.map((v) => v.streetName);
+	const lastNames = userData.data.map((v) => v.lastname);
 
-const getEgg = async () => {
-	console.log(`계란 얻는중`);
-	await delay();
-	return "🥚";
+	const combined = streetNames.map((v, i) => {
+		return { address: v, user: lastNames[i] };
+	});
+
+	return combined;
 };
-const getHatch = async (egg) => {
-	console.log(`${egg} 부화중`);
-	await delay();
-	return "🐣";
-};
-const getGrow = async (egg) => {
-	console.log(`${egg} 성장중`);
-	await delay();
-	return "🐥";
-};
-const getRoostar = async (egg) => {
-	console.log(`${egg} 성장`);
-	await delay();
-	return "🐓";
-};
-const getChicken = async (egg) => {
-	console.log(`${egg} 도축`);
-	await delay();
-	return "🍗";
-};
-const makeChicken = async () => {
-	const egg = await getEgg();
-	const hatch = await getHatch(egg);
-	const grow = await getGrow(hatch);
-	const roostar = await getRoostar(grow);
-	const chicken = await getChicken(roostar);
-	await console.log(chicken);
-};
-makeChicken();
+fetchData(15).then((v) => console.log(v));
